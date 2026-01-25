f02cc4a0d1725ff29a73c1ea28303e3c9371c734
Mateusz Rynczak <mateuszrynczak@Mateuszs-MacBook-Air.local>
2026-01-25 15:36:34 +0100

    improve

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,6 @@ import "../styles/Result.css";
 function Result({ onNext }) {
   return (
     <div className="result">
-      <p>🎉 Gratulacje! Dobra odpowiedź.</p>
       <button onClick={onNext}>Następne pytanie</button>
     </div>
   );
